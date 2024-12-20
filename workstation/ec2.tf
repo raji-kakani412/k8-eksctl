75e41c0eeeab63784182777c199a6ac2eedef487
rajikakani412 <raji.kakani@gmail.com>
2024-12-20 10:23:07 +0530

    updated

--- workstation/ec2.tf
+++ workstation/ec2.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "allow_all_k8"{
 
 resource "aws_instance" "workspace" {
 
-  ami = data.aws_ami.joindevops.id
+  ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_k8.id]
   tags = {
